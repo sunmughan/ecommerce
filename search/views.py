985d0c6cb64b21a3d15288270eef33ca952d0330
Islombek77 <mislombek27@gmail.com>
2020-08-25 04:23:09 +0500

    Search with Q is added

--- search/views.py
+++ search/views.py
@@ -19,7 +19,7 @@ class SearchProductView(ListView):
         print(method_dict)
         query = method_dict.get('q', None)
         if query is not None:
-            print(query)
-            return Product.objects.filter(title__icontains=query)
+            return Product.objects.search(query)
+            # return Product.objects.filter(title__icontains=query)
         return Product.objects.featured()
 
